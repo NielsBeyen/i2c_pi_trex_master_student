7534f75f14f90bc884ad205ad1daaef7980691e1
Ruben Dep <rubendepecker@hotmail.com>
2014-11-28 11:38:29 +0100

    pull request

--- MyStatusDataPacket.cpp
+++ MyStatusDataPacket.cpp
@@ -4,17 +4,17 @@ namespace TRexLib{
 
 	
     void std::string MytoJSON(void){
-		return 0;
+		
 
     };
 	void MyfromJSON(std::string json){
-		return 0;
+		
 
 	};
 
 	void MytoTRex(char * buffer){
 
-		return 0;
+		
 	};
 	
 }
